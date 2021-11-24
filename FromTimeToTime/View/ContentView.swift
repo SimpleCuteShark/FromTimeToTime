a472f612f3ddfdb58bd575c5b362bc8975700a56
Alex <49230683+Stea11er@users.noreply.github.com>
2021-11-24 19:07:52 +0300

    Добавил переход на экран создания новой задачи Добавить возможность добавлять - удалять задачи из бд

--- FromTimeToTime/View/ContentView.swift
+++ FromTimeToTime/View/ContentView.swift
@@ -15,30 +15,49 @@ struct ContentView: View {
     
     @State private var newTask = ""
     
-
+    
     var body: some View {
-        List {
-            Section(header: Text("Добавить задачу"), content: {
-                HStack {
-                    TextField("Название задачи", text: $newTask)
-                    Button(action: {
-                        
-                    }, label: {
-                        Image(systemName: "plus.circle.fill")
-                    })
-                }
-            })
-            Section(header: Text("Ваши задачи"), content: {
-                ForEach(self.tasks, id: \.self) { task in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(task.name).font(.headline)
-                            Text("\(task.createdDate)").font(.caption)
+        NavigationView {
+            List {
+                Section(header: Text("Добавить задачу"), content: {
+                    GeometryReader() {_ in
+                        HStack {
+                            TextField("Название задачи", text: $newTask)
+                            Spacer()
+                            if newTask == "" {
+                                NavigationLink(destination: NewTaskView()) {
+                                    Text("Создать задачу")
+                                } } else {
+                                    Button(action: {
+                                    }, label: {
+                                        Image(systemName: "plus.circle.fill")
+                                    })
+                                }
                         }
                     }
-                }
-            })
+                })
+                Section(header: Text("Ваши задачи"), content: {
+                    ForEach(self.tasks, id: \.self) { task in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(task.name).font(.headline)
+                                Text("\(task.createdDate)").font(.caption)
+                            }
+                        }
+                    }
+                })
+            }.navigationBarTitle(Text("Доска задач"))
+            // .navigationBarItems(trailing: EditButton())
         }
+    }
+}
+
+struct NewTaskView: View {
+    @State private var govno = ""
+    @State private var mocha = ""
+    
+    var body: some View {
+        Text("test")
     }
 }
 
